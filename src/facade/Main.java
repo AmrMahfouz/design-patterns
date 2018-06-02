ba8e709086456725cbd5bccf3a584d9a5f51010c
Amr Mahfouz <eng.csed.amr.mahfouz@gmail.com>
2018-06-03 01:24:01 +0200

    Facade Example

---
+++ src/facade/Main.java
@@ -0,0 +1,14 @@
+package facade;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ShapeMaker shapeMaker = new ShapeMaker();
+        shapeMaker.drawCircle();
+        shapeMaker.drawSquare();
+        shapeMaker.drawRectangle();
+
+    }
+
+}
